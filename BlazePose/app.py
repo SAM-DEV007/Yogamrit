8d44487a7a0e85505ba8e29dc932d8795e61bd48
Tsu-Giriiii <153367525+Tsu-Giriiii@users.noreply.github.com>
2025-03-08 22:37:38 +0530

    Update app.py

--- BlazePose/app.py
+++ BlazePose/app.py
@@ -1,31 +1,44 @@
-#!/usr/bin/env python
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response,jsonify
 import cv2
-import sys
-import numpy
+from core_logic_reframed import process_frame, calculate_angle,preprocess_data,landmark_list  # Import core logic
 
 app = Flask(__name__)
+
+
+
+def generate_frames():
+    cap = cv2.VideoCapture(0)  # Open webcam
+    global current__asana
+
+    while True:
+        
+
+        frame,prev_text = process_frame(cap)  # Pass frame to process_frame()
+        current__asana = prev_text
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        if not ret:
+            break
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+    cap.release()
+
 
 @app.route('/')
 def index():
     return render_template('test.html')
 
-def get_frame():
-    camera_port=0
-    camera=cv2.VideoCapture(camera_port) #this makes a web cam object
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    while True:
-        retval, im = camera.read()
-        imgencode=cv2.imencode('.jpg',im)[1]
-        stringData=imgencode.tostring()
-        yield (b'--frame\r\n'
-            b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
-
-
-@app.route('/calc')
-def calc():
-     return Response(get_frame(),mimetype='multipart/x-mixed-replace; boundary=frame')
-
+@app.route('/asana_name')
+def asana_name():
+    return jsonify({"asana": current__asana})
+    
 
 if __name__ == '__main__':
-    app.run(host='localhost', debug=True, threaded=True)
+    app.run(debug=True)
